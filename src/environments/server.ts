c216306aac18a6efa685397cf96ce6c5a1875891
abel1313 <56236714+abel1313@users.noreply.github.com>
2025-10-25 11:22:40 -0600

    url contrada

--- src/environments/server.ts
+++ src/environments/server.ts
@@ -4,6 +4,6 @@
 
 export class ServerAPI{
 
-    public static serverApi = 'http://localhost:8081/mis-productos';
-    //public static serverApi = 'https://proyecto-key-1.onrender.com/mis-productos';
+    //public static serverApi = 'http://localhost:8081/mis-productos';
+    public static serverApi = 'https://proyecto-key-1.onrender.com/mis-productos';
 }

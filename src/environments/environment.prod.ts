e26435d1d4a50c8088d7d5c89a1443e11e227255
abel1313 <56236714+abel1313@users.noreply.github.com>
2025-11-16 13:31:47 -0600

    Se regresa el endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: true,
-  //api_Url: 'https://proyecto-key-1.onrender.com/mis-productos'
-  api_Url: 'https://71bfb71f-e8f3-403b-bb8d-1f490a8d38bf.cfargotunnel.com/mis-productos'
+  api_Url: 'https://proyecto-key-1.onrender.com/mis-productos'
+  //api_Url: 'https://71bfb71f-e8f3-403b-bb8d-1f490a8d38bf.cfargotunnel.com/mis-productos'
 };

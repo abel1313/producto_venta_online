01bd9cf8111adf35150be99b88b4d60574629d45
abel1313 <56236714+abel1313@users.noreply.github.com>
2025-11-16 14:13:54 -0600

    cambio endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 
 export const environment = {
   production: true,
-  api_Url: 'https://proyecto-key-1.onrender.com/mis-productos'
+    //api_Url: 'https://proyecto-key-1.onrender.com/mis-productos'
+    api_Url: 'https://api.novedades-jade.com.mx/mis-productos'
   //api_Url: 'https://71bfb71f-e8f3-403b-bb8d-1f490a8d38bf.cfargotunnel.com/mis-productos'
 };

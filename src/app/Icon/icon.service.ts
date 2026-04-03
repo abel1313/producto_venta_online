33e96057a2990a88c71ddbc325fb23a4b5885957
abel1313 <56236714+abel1313@users.noreply.github.com>
2026-04-03 01:30:04 -0600

    subiendo

--- src/app/Icon/icon.service.ts
+++ src/app/Icon/icon.service.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class IconService {
   iconCarrito = 'assets/iconos/cart.png';
+  iconRegresar = 'assets/iconos/regresar.webp';
+  iconLimpiar = 'assets/iconos/clean.png';
   iconEliminar = 'assets/iconos/delete.png';
   iconEditar = 'assets/iconos/refresh.png';
   iconSave = 'assets/iconos/diskette.png';

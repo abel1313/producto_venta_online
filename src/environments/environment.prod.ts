d24203163132d45c847e0e17e364559698ebaf36
abel1313 <56236714+abel1313@users.noreply.github.com>
2026-04-19 01:57:06 -0600

    se agrego el cambio a produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: true,
     //api_Url: 'https://proyecto-key-1.onrender.com/mis-productos'
-    api_Url: 'http://51.178.29.99:30010/mis-productos',
+    api_Url: 'https://backend.novedades-jade.com.mx/mis-productos',
     api_imagenes: 'http://localhost:30096/mis-productos'
 };
